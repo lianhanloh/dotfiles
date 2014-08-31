7201973cdacb809d8b6a5bdaf8ed6ac5b6e0a34d
Lianhan Loh <loh.lianhan@gmail.com>
2014-08-31 10:30:42 -0400

    .ssh/config set up included

--- set_up.sh
+++ set_up.sh
@@ -1,4 +1,8 @@
-# this is a script to set up symlinks for .vimrc, .bashrc and .gitconfig files if absent.
+# this is a script to set up symlinks for the following files if absent.
+# .bashrc
+# .gitconfig
+# .vimrc
+# .ssh/config
 
 echo "setting up symlinks for .vimrc, .bashrc and .gitconfig"
 pushd ~
@@ -23,6 +27,15 @@ then
 else 
 	echo ".gitconfig symlink exists"
 fi
+if [ ! -d .ssh ]
+then
+	mkdir .ssh
+fi
+if [ ! -e .ssh/config ]
+	ln -sv ~/dotfiles/.ssh/config .ssh/config
+	echo ".ssh/config symlink created"
+else
+	echo ".ssh/config symlink exists"
+fi
 popd
 echo "done"
-
